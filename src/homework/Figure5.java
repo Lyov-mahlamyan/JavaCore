58e33dc3dcdab15af82ff15513de6b9ebd440c64
Levon Mahlamyan <mahlamyan@gmail.com>
2022-05-14 23:59:04 +0300

    All Example

---
+++ src/homework/Figure5.java
@@ -0,0 +1,33 @@
+package homework;
+
+public class Figure5 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 4; i++) {
+            for (int j = 9; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        for (int i = 6; i < 9; i++) {
+            for (int j = 0; j <=i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 9; k > i; k--)
+                System.out.print("* ");
+            System.out.println();
+        }
+
+
+
+    }
+
+}
+
+
+
+
+
